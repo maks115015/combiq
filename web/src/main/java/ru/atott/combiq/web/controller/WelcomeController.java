d61dbeb84e8ad964f8ca55ba4eb7029280fe88b8
atott <atott@yandex.ru>
2015-04-07 22:27:00 +0300

    Migration to spring-data elasticsearch as repository layer

--- web/src/main/java/ru/atott/combiq/web/controller/WelcomeController.java
+++ web/src/main/java/ru/atott/combiq/web/controller/WelcomeController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import ru.atott.combiq.dao.entity.UserEntity;
-import ru.atott.combiq.dao.repository.UserRepository;
 import ru.atott.combiq.service.question.GetQuestionContext;
 import ru.atott.combiq.service.question.GetQuestionResponse;
 import ru.atott.combiq.service.question.GetQuestionService;
@@ -15,13 +13,9 @@ import ru.atott.combiq.service.question.GetQuestionService;
 public class WelcomeController extends BaseController {
     @Autowired(required = false)
     private GetQuestionService getQuestionService;
-    @Autowired
-    private UserRepository userRepository;
 
     @RequestMapping(value = {"/", "index.do"}, method = RequestMethod.GET)
     public ModelAndView index() {
-        UserEntity one = userRepository.findOne("1");
-
         GetQuestionContext context = getContext(GetQuestionContext.class);
         context.setSize(400);
         GetQuestionResponse questions = getQuestionService.getQuestions(context);
